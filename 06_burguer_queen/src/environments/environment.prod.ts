1f364da9567964d6316c9893e7cfdc42925de3b6
EvelynDFloresL <eve_flor17@outlook.com>
2025-05-15 23:58:37 -0600

    Aplicacion de Burguer Queen Terminada sin llaves de api

---
+++ 06_burguer_queen/src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+/* export const environment = {
+  production: true,
+  urlApi: 'http://192.168.1.133:3000/api/v1/',
+  publishKey: '',
+  secretKey: ''
+};
+ */
+
+export const environment = {
+  production: true,
+  urlApi: 'http://192.168.100.11:3000/api/v1/',//pegamos la ip de nuestra pc
+  publishKey: '',//Cambiar llaves
+  secretKey: ''
+
+};
